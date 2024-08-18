30af22e5e39c71ef00e5ceb585f40f4576e27fde
Redkookieee <79647875+richardmanzo@users.noreply.github.com>
2024-08-18 20:18:59 +0800

    initial commit

---
+++ products.js
@@ -0,0 +1,97 @@
+const products = [
+    {
+        "id": 1,
+        "name": "Lugaw",
+        "price": 50,
+        "description": "egg, rice soup",
+        "images": "images/lugaw.png"
+    },
+    {
+        "id": 2,
+        "name": "Sopas",
+        "price": 20,
+        "description": "*gb ram, 251 gb",
+        "images": "images/sopas.jpg"
+    },
+    
+    {
+        "id": 3,
+        "name": "Gulay",
+        "price": 70,
+        "description": "*gb ram, 251 gb",
+        "images": "images/ampalaya.jpg"
+    },
+    {
+        "id": 4,
+        "name": "Kanin",
+        "price": 10,
+        "description": "*gb ram, 251 gb",
+        "images": "images/kanin.jpg"
+    },
+    {
+        "id": 5,
+        "name": "Pares",
+        "price": 100,
+        "description": "*gb ram, 251 gb",
+        "images": "images/pares.jpg"
+    },
+    {
+        "id": 6,
+        "name": "Fried Chicken",
+        "price": 50,
+        "description": "Fried",
+        "images": "images/fried chicken.jpg"
+    },
+    {
+        "id": 7,
+        "name": "Kape",
+        "price": 20,
+        "description": "*gb ram, 251 gb",
+        "images": "images/kape.jpg"
+    },
+    {
+        "id": 8,
+        "name": "Gatas",
+        "price": 20,
+        "description": "*gb ram, 251 gb",
+        "images": "images/gatas.jpg"
+    },
+    {
+        "id": 8,
+       "name": "Fried Chicken",
+        "price": 50,
+        "description": "Fried",
+        "images": "images/fried chicken.jpg"
+    },
+    {
+        "id": 8,
+        "name": "Gatas",
+        "price": 20,
+        "description": "*gb ram, 251 gb",
+        "images": "images/gatas.jpg"
+    },
+    {
+        "id": 8,
+        "name": "Gatas",
+        "price": 20,
+        "description": "*gb ram, 251 gb",
+        "images": "images/gatas.jpg"
+    },
+    {
+        "id": 8,
+        "name": "Gatas",
+        "price": 20,
+        "description": "*gb ram, 251 gb",
+        "images": "images/gatas.jpg"
+    },
+    {
+        "id": 8,
+        "name": "Gatas",
+        "price": 20,
+        "description": "*gb ram, 251 gb",
+        "images": "images/gatas.jpg"
+    },
+
+];
+export default products;
+  
